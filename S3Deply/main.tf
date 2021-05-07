3bc4164deb271696769fcfb0fc3bd7a9e01987f6
=Thanes-learn <=Thanes.learn@gmail.com>
2021-05-08 02:17:49 +0530

    s3 config Added

--- S3Deply/main.tf
+++ S3Deply/main.tf
@@ -1,3 +1,17 @@
 provider "aws" {
   region = "us-east-2"
 }
+
+variable "s3_bucket_name" {
+   type = "list"
+   default = ["html", "scipts", "backup_logs"]
+}
+resource "aws_s3_bucket" "sourceBucket" {
+   count = "${length(var.s3_bucket_name)}"
+   bucket = "${var.s3_bucket_name[count.index]}"
+   acl = "public"
+   versioning {
+      enabled = true
+   }
+   force_destroy = "true"
+}
