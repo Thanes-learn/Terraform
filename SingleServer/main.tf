a91669a882658b4c011fa1f842a1289e40dca438
=Thanes-learn <=Thanes.learn@gmail.com>
2021-05-08 18:55:40 +0530

    Ec2 Instance access added by key pair

--- SingleServer/main.tf
+++ SingleServer/main.tf
@@ -5,16 +5,20 @@ resource "aws_instance" "web" {
   ami                    = "ami-077e31c4939f6a2f3"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.webSecurity.id]
-  user_data              = <<EOF
+  key_name = "adminSSH"
+ user_data       = <<EOF
     #!/bin/bash
-    yum -y update
+	yum -y update
+	yum -y install openssl
+	useradd -p $(openssl passwd -1 admin@123) admin
     yum -y install httpd
-    MYIP=$hostname
-    echo "<h2>Hello World</h2><br>$MYIP">/var/www/html/index.html
+    MYIP=$HOSTNAME
+    echo "<h1>Hello World</h1><h2>  $HOSTNAME  </h1> ">/var/www/html/index.html
     service httpd start
     service httpd enabled
     chkconfih httpd on
     EOF
+
   tags = {
     Name  = "webserver built by thanesh"
     Owner = "Thanesh"
@@ -60,4 +64,9 @@ resource "aws_security_group" "webSecurity" {
     Owner = "Thanesh"
   }
   
+}
+
+output "webinstance_ip" {
+  description = "List of IDs of instances"
+  value       = aws_instance.web.public_ip
 }
--- main.tf
+++ main.tf
@@ -1,44 +1,72 @@
 provider "aws" {
   region = "us-east-2"
 }
+resource "aws_instance" "web" {
+  ami                    = "ami-077e31c4939f6a2f3"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.webSecurity.id]
+  key_name = "adminSSH"
+ user_data       = <<EOF
+    #!/bin/bash
+	yum -y update
+	yum -y install openssl
+	useradd -p $(openssl passwd -1 admin@123) admin
+    yum -y install httpd
+    MYIP=$HOSTNAME
+    echo "<h1>Hello World</h1><h2>  $HOSTNAME  </h1> ">/var/www/html/index.html
+    service httpd start
+    service httpd enabled
+    chkconfih httpd on
+    EOF
 
-variable "s3_bucket_name" {
-   type = list(string)
-   default = ["html123456", "scipts123456"]
+  tags = {
+    Name  = "webserver built by thanesh"
+    Owner = "Thanesh"
+  }
+
+
+
 }
-resource "aws_s3_bucket" "mainbucket" {
-   count = "${length(var.s3_bucket_name)}"
-   bucket = "${var.s3_bucket_name[count.index]}"
-   acl = "public-read"
-   versioning {
-      enabled = true
-   }
-   force_destroy = "true"
-}
-resource "aws_s3_bucket" "sourcebucket" {
-   
-   bucket = "backuplogs123456"
-   acl = "public-read-write"
-   versioning {
-      enabled = true
-   }
-   force_destroy = "true"
-}
+resource "aws_security_group" "webSecurity" {
+  name = "WebServerACL"
 
-data "aws_s3_bucket" "testbucket" {
-  bucket="backuplogs123456"
-}
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-resource "aws_s3_bucket_object" "uploadingHTML" {
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-  bucket = data.aws_s3_bucket.testbucket.id
+ 
+ 
 
-  key    = "profile.html"
-
-  acl    = "public-read-write"  # or can be "public-read"
-
-  source = "index.html"
-
+    ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name  = "webserver SG built by thanesh"
+    Owner = "Thanesh"
+  }
   
+}
 
+output "webinstance_ip" {
+  description = "List of IDs of instances"
+  value       = aws_instance.web.public_ip
 }
