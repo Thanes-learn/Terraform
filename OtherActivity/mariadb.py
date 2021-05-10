e7b5ce7288f1bf8f91334327590547f900c6725f
=Thanes-learn <=Thanes.learn@gmail.com>
2021-05-11 03:23:25 +0530

    Unwated test files build cache has been removed

--- OtherActivity/mariadb.py
+++
@@ -1,6 +0,0 @@
-import os
-import mysql.connector as database
-connection = database.connect(
-    user="root",
-    password="rtsawsadmin",
-    host="myinstance.c065vc4pnlfy.us-east-2.rds.amazonaws.com")
--- OtherActivity/mysqlpy.py
+++
@@ -1,2 +0,0 @@
-import mysql.connector as cn
-con=cn.connect(host="myinstance.c065vc4pnlfy.us-east-2.rds.amazonaws.com",user="root",password="rtsawsadmin")
