6541155b6b247b6ada7a71e409b565549677d741
=Thanes-learn <=Thanes.learn@gmail.com>
2021-05-11 03:15:23 +0530

    RDS Server Creation by Terraform

--- RDSServer/main.tf
+++ RDSServer/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 
-#Security Group for Allowing traffic
+#Security Group for Allowing traffic for web
 resource "aws_security_group" "webSecurity" {
   name = "WebServerACL"
 
@@ -44,41 +44,16 @@ resource "aws_security_group" "webSecurity" {
   }
   
 }
-#Web Server Configration
-resource "aws_instance" "web" {
-  ami                    = "ami-077e31c4939f6a2f3"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.webSecurity.id]
-  key_name = "adminSSH"
- user_data       = <<EOF
-    #!/bin/bash
-	yum -y update
-	yum -y install openssl
-	useradd -p $(openssl passwd -1 admin@123) admin
-    yum -y install httpd
-    MYIP=$HOSTNAME
-    echo "<h1>Hello World</h1><h2>  $HOSTNAME  </h1> ">/var/www/html/index.html
-    service httpd start
-    service httpd enabled
-    chkconfih httpd on
-    EOF
-
-  tags = {
-    Name  = "webserver built by thanesh"
-    Owner = "Thanesh"
-  }
-}
-
-
 
 #RDS configration
 resource "aws_db_instance" "mydb" {
   allocated_storage        = 10 # gigabytes
   engine                   = "mysql"
+  engine_version       = "5.7"
   identifier               = "mydb"
   instance_class           = "db.t2.micro"
   name                     = "mydb"
-  password                 = "admin@123"
+  password                 = "rtsawsadmin"
   port                     = 3306
   publicly_accessible      = true
   username                 = "admin"
@@ -88,9 +63,4 @@ resource "aws_db_instance" "mydb" {
 output "address" {
   value       = aws_db_instance.mydb.address
   description = "Connect to the database at this endpoint"
-}
-
-output "port" {
-  value       = aws_db_instance.mydb.port
-  description = "The port the database is listening on"
 }
